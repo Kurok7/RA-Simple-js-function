3414600aa8859ec7bfcded26a4f3e14e8a7496b7
Juan K Malone <65433150+Kurok7@users.noreply.github.com>
2022-12-28 04:39:50 -0800

    Initialized repo with js file.

---
+++ index.js
@@ -0,0 +1,17 @@
+//using international measurements for height
+
+function user(name, height, country) {
+	alert(
+		"My name is " +
+			name +
+			". I am " +
+			height +
+			"cm tall and I am from " +
+			country +
+			"."
+	);
+}
+
+user("Juan", 172, "America");
+user("Cherry", 154, "Thailand");
+user("Daniel", 160, "Ethiopia");
